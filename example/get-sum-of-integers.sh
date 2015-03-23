ef82cd97585ac5df6a315380d72043a75621d5a6
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-03-23 19:51:36 -0400

    fixed script file

--- example/get-sum-of-integers.sh
+++ example/get-sum-of-integers.sh
@@ -6,7 +6,7 @@ export PATH=$SPARK_HOME:$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH
 
 OWN_DIR=`dirname "${BASH_SOURCE[0]}"`
 OWN_DIR_ABS_PATH=`realpath "$OWN_DIR"`
-JAR=`realpath "$OWN_DIR_ABS_PATH/../../../target/flintstone-0.0.1-SNAPSHOT.jar"`
+JAR=`realpath "$OWN_DIR_ABS_PATH/../target/flintstone-0.0.1-SNAPSHOT.jar"`
 CLASS=org.janelia.flintstone.Example
 
 if [ -z $PARALLELISM ]; then
