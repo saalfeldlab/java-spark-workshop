25cb63e575f8b25b869e718c6fda1e226f5b4f0a
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-03-24 11:52:13 -0400

    added comment on inflame.sh

--- example/example.sh
+++ example/example.sh
@@ -1,3 +1,6 @@
+# this example can also be run like:
+# ../src/main/shell/inflame.sh <N_NODES> ../target/flintstone-0.0.1-SNAPSHOT.jar org.janelia.flintstone.Example <N>
+
 N=${N:-20}
 ARGV=$N
 N_NODES=${N_NODES:-5}
