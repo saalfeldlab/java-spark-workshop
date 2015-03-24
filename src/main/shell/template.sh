f28fd6c9aa0373c67949725d58232c026252b941
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-03-24 11:37:44 -0400

    added inflame.sh and template.sh

---
+++ src/main/shell/template.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+export SPARK_HOME=${SPARK_HOME:-/usr/local/spark-current}
+export PATH=$SPARK_HOME:$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH
+
+JAR=`realpath "%s"`
+CLASS=%s
+
+if [ -z $PARALLELISM ]; then
+    echo "PARALLELISM not defined."
+    exit 1
+fi
+
+if [ -z $MASTER ]; then
+    echo "MASTER not defined."
+    exit 1
+fi
+
+$SPARK_HOME/bin/spark-submit --verbose \
+    --conf spark.default.parallelism=$PARALLELISM \
+    --class $CLASS \
+    $JAR $@
