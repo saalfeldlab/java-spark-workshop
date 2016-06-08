45b4947c22b29e5e1eb9adb0592ac8092aecd1c4
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-06-08 14:46:56 -0400

    updated good-practice-image-pairs.sh to use latest flintstone

--- good-practice-image-pairs.sh
+++ good-practice-image-pairs.sh
@@ -1,5 +1,9 @@
-INFLAME=$PWD/inflame.sh
-JAR=$PWD/target/java-spark-workshop-0.0.1-SNAPSHOT.jar # this jar must be accessible from the cluster
+OWN_DIR=`dirname "${BASH_SOURCE[0]}"`
+ABS_DIR=`readlink -f "$OWN_DIR"`
+
+
+FLINTSTONE=$ABS_DIR/flintstone/flintstone.sh
+JAR=$ABS_DIR/target/java-spark-workshop-0.0.1-SNAPSHOT.jar # this jar must be accessible from the cluster
 CLASS=org.janelia.workshop.spark.practices.PairwiseImageSelection
 N_NODES=40
 
@@ -8,4 +12,4 @@ SIDE_LENGTH=1000
 RANGE=300
 ARGV="$N_IMAGES $SIDE_LENGTH $RANGE"
 
-$INFLAME $N_NODES $JAR $CLASS $ARGV
+TERMINATE=1 $FLINTSTONE $N_NODES $JAR $CLASS $ARGV
